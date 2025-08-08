842c34625e13dcf94560dc6af2d48fd78489c206
qalib01 <qalib.mmmdli@gmail.com>
2025-08-08 17:53:01 +0400

    feat: Actvity component added

---
+++ src/components/card/Activity.tsx
@@ -0,0 +1,34 @@
+import { ArrowUpRight } from "lucide-react";
+
+interface PageProp {
+    title: string;
+    img: string;
+}
+
+const ActivityCard = ({ title, img }: PageProp) => {
+    return (
+        <div className="flex w-full flex-col relative">
+            <div className="flex-1 gap-8 flex flex-col relative">
+                <img
+                    src={img}
+                    className="aspect-square rounded-4xl object-cover"
+                />
+                <div className="absolute top-0 right-0 bg-violet-500 rounded-full p-4 z-5">
+                    <ArrowUpRight size={32} color="white" />
+                </div>
+                <div className="bg-white h-20 w-20 rounded-bl-4xl absolute top-0 right-0 z-4" />
+                <div className="bg-transparet h-20 w-20 rounded-br-4xl absolute top-[80px] right-0 z-4 corner-white top-right-corner" />
+                <div className="bg-transparet h-20 w-20 rounded-br-4xl absolute top-0 right-[80px] z-4 corner-white top-right-corner" />
+            </div>
+            
+            <div className="bg-violent-500/50 h-full rounded-b-4xl w-full absolute bottom-0 left-0" style={{ background: 'linear-gradient(0deg,rgba(124, 88, 255, 0.5) 0%, rgba(27, 27, 27, 0) 100%)' }}></div>
+
+            <div className="gap-4 mt-4 absolute bottom-7 left-5 text-white">
+                <h6 className="text-4xl font-medium"> {title} </h6>
+            </div>
+        </div>
+    );
+};
+
+
+export default ActivityCard;
--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,3 +1,4 @@
+import ActivityCard from "../../components/card/Activity";
 import ChallengeCard from "../../components/card/Challenge";
 import CourseCard from "../../components/card/Course";
 import HeroSection from "../../components/hero/Hero";
@@ -7,18 +8,29 @@ const Home = () => {
     return (
         <>
             <HeroSection />
-            <SimpleSection title='Challenges'>
-                <ChallengeCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur." img="/media/icons/phones.png" />
-                <ChallengeCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur." img="/media/icons/clients.png" />
+            <SimpleSection title="Challenges">
+                {
+                    [0, 1].map(() => (
+                        <ChallengeCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur." img="/media/icons/phones.png" />
+                    ))
+                }
             </SimpleSection>
-            {/* <SimpleSection title='Experts' description="Lorem ipsum dolor sit amet consectetur. Purus tincidunt sapien vitae libero malesuada.Lorem ipsum dolor sit amet consectetur. Purus tincidunt sapien vitae libero malesuada.">
-                <ChallengeCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur." img="/media/icons/phones.png" />
-                <ChallengeCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur." img="/media/icons/clients.png" />
-            </SimpleSection> */}
-            <SimpleSection title='Courses' description="Lorem ipsum dolor sit amet consectetur. Aliquam nibh nulla in neque at pretium. Massa metus morbi eu nunc">
-                <CourseCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur. Mi et consectetur vestibulum nunc. Tincidunt est nec sollicitudin mauris in." img="/media/images/course.jpg" />
-                <CourseCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur. Mi et consectetur vestibulum nunc. Tincidunt est nec sollicitudin mauris in." img="/media/images/course.jpg" />
-                <CourseCard title="Challenge Name" description="Lorem ipsum dolor sit amet consectetur. Mi et consectetur vestibulum nunc. Tincidunt est nec sollicitudin mauris in." img="/media/images/course.jpg" />
+            <SimpleSection title="Courses" description="Lorem ipsum dolor sit amet consectetur. Aliquam nibh nulla in neque at pretium. Massa metus morbi eu nunc">
+                {
+                    [0, 1, 2].map(() => (
+                        <CourseCard title="Course Name" description="Lorem ipsum dolor sit amet consectetur. Mi et consectetur vestibulum nunc. Tincidunt est nec sollicitudin mauris in." img="/media/images/course.jpg" />
+                    ))
+                }
+            </SimpleSection>
+            <SimpleSection title="Activities">
+                {
+                    [0, 1, 2, 3].map(() => (
+                        <ActivityCard title="Challenge" img="/media/images/course.jpg" />
+                    ))
+                }
+            </SimpleSection>
+            <SimpleSection title="More than 37 companies cooperate with us">
+                <></>
             </SimpleSection>
         </>
     )
